b335e2f591541acb74c0a7d7af8f5df6c7a208d1
Andrew Holt <ajh.206@outlook.com>
2019-10-04 15:01:55 -0700

    Update Chunk.ps1
    Added logging functionality

--- Chunk.ps1
+++ Chunk.ps1
@@ -1,3 +1,14 @@
+param($currDate = (Get-Date), 
+       $Logfile = "PATH_TO_LOGS\log\"+$currDate.toString("yyyy-MM-dd HH-mm-ss")+"_test.log"
+      )
+
+# Logging Schema [Date - Time - Function Step]  
+Function LogWrite{
+    Param ([string]$logstring, $session)
+    $Stamp = $currDate.toString("yyyy/MM/dd HH:mm:ss")
+    Add-content $Logfile -value ('{0} {1}' -f $Stamp, $logstring)
+}
+
 #Chunksize Mimicry: From a master list, iterate over chunks of N files in folder/directory for ETL use 
 function Chunks{
     param($All_Files, $ChunkSize)
